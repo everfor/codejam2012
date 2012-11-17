15451919a10cf364a1316340564a2732bfbea8ce
everfor <alan.yang1994@gmail.com>
2012-11-17 02:55:46 -0500

    PriceFeed class updated

--- test/pr.py
+++ test/pr.py
@@ -1,43 +1,42 @@
 import socket
-import time
-import sqlite3 as sqlite
-import sys
-import time
 import DataBase as DB
 
 IP = '127.0.0.1'
 PORT = 8211
 
-pr_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Create a database:
-db = DB.DataBase()
+class PriceFeed:
+    pr_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, IP, PORT):
+        self.IP = IP
+        self.PORT = PORT
+        #Create a database:
+        self.db = DB.DataBase()
+    
+    def connect(self):
+        #Connect to the server:
+        self.pr_sock.connect((IP, PORT))
+        print 'Connecting to ' + str(PORT)
+    
+    def startFeed(self):
+        #Sned an "H" which means starting the communication:
+        self.pr_sock.send('H\r\n')
 
-#Connect to the server:
-pr_sock.connect((IP, PORT))
-print 'Connecting to ' + str(PORT)
+        #Create a table:
+        self.db.createTable()
 
-#Sned an "H" which means starting the communication:
-pr_sock.send('H\r\n')
-
-#Create a table:
-db.createTable()
-
-index=0
-price=''
-initial=time.time()
-while(1):
-    try:
-        data = pr_sock.recv(1)
-        #When it's a "C" all ends:
-        if(data == 'C'):
-            break
-        elif(data == '|'):
-            db.insertData(price)
-            price = ''
-            continue
-        price += data
-    except:
-        print 'Exception occurred during the reception of data.'
-db.showValues()
-delta_time = time.time() - initial
-print delta_time
+        index=0
+        price=''
+        while(1):
+            try:
+                data = pr_sock.recv(1)
+                #When it's a "C" all ends:
+                if(data == 'C'):
+                    break
+                elif(data == '|'):
+                    self.db.insertData(price)
+                    price = ''
+                    continue
+                price += data
+            except:
+                print 'Exception occurred during the reception of data.'
+        self.db.showValues() #Disable it after coding it all up.
